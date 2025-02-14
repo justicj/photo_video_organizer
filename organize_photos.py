4b2165ef57ad3f785345f6e4ad437e981e638897
Jason Justice <jayjustice@gmail.com>
2025-02-14 13:43:46 -0800

    organize

--- organize_photos.py
+++ organize_photos.py
@@ -2,6 +2,7 @@ import os
 from datetime import datetime
 import argparse
 import subprocess
+import sys
 import json
 import requests
 from PIL import Image
@@ -15,10 +16,15 @@ FFPROBE_URL = "https://github.com/BtbN/FFmpeg-Builds/releases/download/latest/ff
 FFPROBE_PATH = "./ffmpeg-master-latest-win64-gpl-shared/bin/ffprobe.exe"
 
 
+def get_date_from_timestamp(file):
+    print(f"Date determined by file creation time: {file}")
+    creation_time = os.path.getctime(file)
+    return datetime.fromtimestamp(creation_time).strftime("%Y:%m:%d %H:%M:%S")
+
+
 def get_photo_exif_date(file):
     with Image.open(file) as img:
         file_ext = os.path.splitext(file)[1]
-        # print(f"file: {file}")
         if file_ext.lower() == ".heic":
             exif_data = img.getexif()
         else:
@@ -28,11 +34,8 @@ def get_photo_exif_date(file):
                 tag_name = TAGS.get(tag, tag)
                 if tag_name == "DateTimeOriginal":
                     return value
-        print(f"Date determined by file creation time: {file}")
-        creation_time = os.path.getctime(file)
-        return datetime.fromtimestamp(creation_time).strftime(
-            "%Y:%m:%d %H:%M:%S"
-        )
+        # if nothing is found, return the file creation time
+        return get_date_from_timestamp(file)
 
 
 def get_mov_exif_date(video_path):
@@ -64,10 +67,8 @@ def get_mov_exif_date(video_path):
         return None, None
 
 
-def extract_date(date_str, date_format):
+def format_date(date_str, date_format):
     if date_str is not None and date_str != "0000:00:00 00:00:00":
-        # print(f"file date format: {date_str}")
-        # print(f"Expected format: {date_format}")
         try:
             date = datetime.strptime(date_str, date_format)
             return date
@@ -77,47 +78,48 @@ def extract_date(date_str, date_format):
     return None
 
 
-def organize_file(file, file_type):
-    if file_type == "photo":
-        date_str = get_photo_exif_date(file)
-        date = extract_date(date_str, "%Y:%m:%d %H:%M:%S")
-    elif file_type == "video":
-        date_str, duration = get_mov_exif_date(file)
-        if duration:
-            if float(duration) < LIVE_PHOTO_DURATION:
-                print(f"Video looks like a live photo: {file}")
-                destination_directory = (
-                    f"{DESTINATION_PICTURE_DIRECTORY}\\live_photo"
-                )
-                os.makedirs(destination_directory, exist_ok=True)
-                os.rename(
-                    file,
-                    os.path.join(
-                        destination_directory, os.path.basename(file)
-                    ),
-                )
-                return True
-        date = extract_date(date_str, "%Y-%m-%dT%H:%M:%S.%fZ")
-        if date is None:
-            date = extract_date(date_str, "%Y-%m-%dT%H:%M:%SZ")
-    if date is not None:
+def move_live_photo(file):
+    print(f"Video looks like a live photo: {file}")
+    destination_directory = f"{DESTINATION_PICTURE_DIRECTORY}\\live_photo"
+    os.makedirs(destination_directory, exist_ok=True)
+    os.rename(
+        file,
+        os.path.join(destination_directory, os.path.basename(file)),
+    )
+    return True
+
+
+def move_file(file, file_type, date):
+    if date is None:
+        destination_directory = (
+            f"{DESTINATION_PICTURE_DIRECTORY}\\{file_type}\\no_date"
+        )
+    else:
         year = date.year
         month = date.month
         destination_directory = f"{DESTINATION_PICTURE_DIRECTORY}\\{file_type}\\{year}\\{month:02d}"
-        os.makedirs(destination_directory, exist_ok=True)
-        os.rename(
-            file, os.path.join(destination_directory, os.path.basename(file))
-        )
-        return True
-    print(f"No date found for {file}")
-    destination_directory = (
-        f"{DESTINATION_PICTURE_DIRECTORY}\\{file_type}\\no_date"
-    )
     os.makedirs(destination_directory, exist_ok=True)
     os.rename(
         file, os.path.join(destination_directory, os.path.basename(file))
     )
-    return False
+    return date is not None
+
+
+def organize_file(file, file_type):
+    date = None
+    if file_type == "photo":
+        date_str = get_photo_exif_date(file)
+        date = format_date(date_str, "%Y:%m:%d %H:%M:%S")
+    elif file_type == "video":
+        date_str, duration = get_mov_exif_date(file)
+        if duration:
+            if float(duration) < LIVE_PHOTO_DURATION:
+                return move_live_photo(file)
+        date = format_date(date_str, "%Y-%m-%dT%H:%M:%S.%fZ")
+        if date is None:
+            date = format_date(date_str, "%Y-%m-%dT%H:%M:%SZ")
+
+    return move_file(file, file_type, date)
 
 
 def download_tools():
@@ -130,33 +132,58 @@ def download_tools():
     os.remove("ffmpeg.zip")
 
 
+def main():
+    successful = []
+    no_date = []
+    unssupported = []
+    result = False
+    source_files = os.listdir(SOURCE_PICTURE_DIRECTORY)
+    print(f"Total files to process: {len(source_files)}")
+    for file in source_files:
+        if file.lower().endswith(tuple(PICTURE_FILE_EXTENSIONS)):
+            result = organize_file(
+                f"{SOURCE_PICTURE_DIRECTORY}\\{file}", "photo"
+            )
+        elif file.lower().endswith(tuple(MOVIE_FILE_EXTENSIONS)):
+            result = organize_file(
+                f"{SOURCE_PICTURE_DIRECTORY}\\{file}", "video"
+            )
+        else:
+            unssupported.append(file)
+            continue
+        if result:
+            successful.append(file)
+        else:
+            no_date.append(file)
+    print(f"Files successfully organized: {len(successful)}")
+    print(f"Files with no date: {len(no_date)}")
+    print(f"Unsupported files: {len(unssupported)}")
+
+
 if __name__ == "__main__":
     register_heif_opener()
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "source",
-        help="Source directory containing photos and videos to organize",
+        "source_directory",
+        help="Source directory containing photos and videos to organize eg: 'F:\\photos'",
     )
     parser.add_argument(
-        "destination",
-        help="Destination directory to move organized photos and videos",
+        "destination_directory",
+        help="Destination directory to move organized photos and videos eg: 'F:\\organized_photos'",
     )
     args = parser.parse_args()
-    # exit with message if either source or destination arg not provided
-    if not args.source or not args.destination:
+    if not args.source_directory or not args.destination_directory:
         print("Please provide source and destination directories")
-        exit()
-    SOURCE_PICTURE_DIRECTORY = args.source
-    DESTINATION_PICTURE_DIRECTORY = args.destination
-    # escape backslashes
+        sys.exit()
+    SOURCE_PICTURE_DIRECTORY = args.source_directory
+    DESTINATION_PICTURE_DIRECTORY = args.destination_directory
     SOURCE_PICTURE_DIRECTORY = SOURCE_PICTURE_DIRECTORY.replace("\\", "\\\\")
     DESTINATION_PICTURE_DIRECTORY = DESTINATION_PICTURE_DIRECTORY.replace(
         "\\", "\\\\"
     )
-    # check if the provided source directory exists
     if not os.path.exists(SOURCE_PICTURE_DIRECTORY):
         print(f"Source directory {SOURCE_PICTURE_DIRECTORY} does not exist")
-        exit()
+        sys.exit()
     print(
         f"Organizing files from {SOURCE_PICTURE_DIRECTORY} to {DESTINATION_PICTURE_DIRECTORY}"
     )
@@ -164,31 +191,4 @@ if __name__ == "__main__":
         import zipfile
 
         download_tools()
-
-    successful = []
-    no_date = []
-    unssupported = []
-    RESULT = False
-    source_files = os.listdir(SOURCE_PICTURE_DIRECTORY)
-    print(f"Total files to process: {len(source_files)}")
-    for file in source_files:
-        if file.lower().endswith(tuple(PICTURE_FILE_EXTENSIONS)):
-            RESULT = organize_file(
-                f"{SOURCE_PICTURE_DIRECTORY}\\{file}", "photo"
-            )
-        elif file.lower().endswith(tuple(MOVIE_FILE_EXTENSIONS)):
-            RESULT = organize_file(
-                f"{SOURCE_PICTURE_DIRECTORY}\\{file}", "video"
-            )
-        else:
-            unssupported.append(file)
-            continue
-        if RESULT:
-            successful.append(file)
-        else:
-            no_date.append(file)
-    print(f"Files successfully organized: {len(successful)}")
-    print(
-        f"Files moved to {DESTINATION_PICTURE_DIRECTORY}\\no_date: {len(no_date)}"
-    )
-    print(f"Unsupported files: {len(unssupported)}")
+    main()
